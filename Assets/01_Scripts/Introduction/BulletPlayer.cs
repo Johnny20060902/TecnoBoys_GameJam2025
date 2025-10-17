2d2a2871b12cfa08f54b36623c2d3dd8b6e8f160
Raul Jimenez Camacho <jcr0034569@est.univalle.edu>
2025-10-16 23:47:29 -0400

    Introduccion mejorada

---
+++ Assets/01_Scripts/Introduction/BulletPlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPlayer : MonoBehaviour
+{
+    public static float moveSpeed = 5;
+    public float timeToDestroy = 5;
+    public static float damage = 1f;
+    //public GameObject explosion;
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Boss"))
+        {
+            Destroy(gameObject);
+            //Instantiate(explosion, transform.position, transform.rotation);
+            //ITakeDamage damageable = collision.gameObject.GetComponent<ITakeDamage>();
+            //if (damageable != null)
+            //{
+            //    damageable.TakeDamage(damage);
+            //    Destroy(gameObject);
+            //}
+        }
+    }
+}
